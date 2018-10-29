2ada9fdec43603c5feeca9b7833bba0c603db4b7
lysww <lyswwr@126.com>
2018-10-29 17:47:23 +0800

    testfffff

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http= require('http');
 
-var httpvale = "shoushou";
+var httpvale = "shoushouman";
 http.createServer(function(req,res)
 {
     res.writeHead(200,{"Content-type":"text/blain"});

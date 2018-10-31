9c87b5a63968023c924b09457463b44e4bad04b4
lysww <lyswwr@126.com>
2018-10-31 14:43:09 +0800

    mongodb 使用

--- db.js
+++ db.js
@@ -1,31 +1,22 @@
 //修改上述 db.js
-var mongoose = require('mongoose'),
+var mongoose = require('mongodb').MongoClient;
     DB_URL = 'mongodb://94.191.33.247:27017/test';
 
 /**
  * 连接
  */
-mongoose.connect(DB_URL);
+mongoose.connect(DB_URL,function (err,clien) {
+    if(!err){
+        console.log("连接成功。");
+    }
 
-/**
- * 连接成功
- */
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose connection open to ' + DB_URL);
 });
 
-/**
- * 连接异常
- */
-mongoose.connection.on('error',function (err) {
-    console.log('Mongoose connection error: ' + err);
-});
+mongoose.disconnect(DB_URL,function (err,clit) {
+    if(!err)
+    {
+        console.log("连接断开");
+    }
 
-/**
- * 连接断开
- */
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose connection disconnected');
-});
-
+})
 module.exports = mongoose;
--- user.js
+++ user.js
@@ -1,14 +1,14 @@
 /**  修改上述 user.js
  * 用户信息
  */
-var mongoose = require('./db.js'),
+var mongoose = require('./db.js');
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     username : { type: String },                    //用户账号
-    userpwd: {type: String},                        //密码
-    userage: {type: Number},                        //年龄
-    logindate : { type: Date}                       //最近登录时间
+    userpwd: {type: String}                    //密码
+//    userage: {type: Number},                        //年龄
+  //  logindate : { type: Date}                       //最近登录时间
 });
 
 module.exports = mongoose.model('User',UserSchema);

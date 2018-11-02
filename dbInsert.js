84f244a36d989fb62cc71dd94eee59cb5863f1d6
lysww <lyswwr@126.com>
2018-11-02 10:52:55 +0800

    nodejs 后台获取html 的参数值，然后入库

--- dbInsert.js
+++ dbInsert.js
@@ -4,9 +4,14 @@ function InsertData(client,arry) {
     console.log("连接成功。");
     const db = client.db("test");
     const pass = db.collection('custom');
+    var dataarr = [];
+      arry.forEach(function (v,i) {
+         console.log(v);
+         dataarr[i] = v;
 
-   // var data = [{"name": "node", "age": 1}, {"name": "test1", "age": 55}];
-    pass.insert(arry, function (err, result) {
+     });
+
+    pass.insert(dataarr, function (err, result) {
         console.log(result);
         client.close();
     });

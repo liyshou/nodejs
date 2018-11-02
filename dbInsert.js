bf75548c2f287e321cbd400873442ba9ee549a58
lysww <lyswwr@126.com>
2018-11-02 17:52:00 +0800

    nodejs 后台获取html 的参数值，然后入库

--- dbInsert.js
+++ dbInsert.js
@@ -1,36 +1,38 @@
 var namevalue;
 var phonevalue;
 var passwordvalue;
+var data;
 function InsertData(client,arry)
 {
       console.log("连接成功。");
-    var value1;
-      arry.forEach(function (v,i)
-      {
-        var value= v.toString().split('=');
-        value.forEach(function (value1,j)
-        {
-             if(i==0 &&j ==1)
-             {
-                console.log("name:" + value1);
-                namevalue  = value1;
-            }
-            else if(i==1 &&j==1)
-             {
-                 console.log("phone:" + value1);
-                 phonevalue  = value1;
-             }
-             else if(i==2 &&j==1)
-             {
-                 console.log("password:" + value1);
-                 passwordvalue  = value1;
-             }
-        });
-      });
+      if(!arry.isArray()) {
+          console.log("接收到的数据是分割的字符串");
+          var value1;
+          arry.forEach(function (v, i) {
+              var value = v.toString().split('=');
+              value.forEach(function (value1, j) {
+                  if (i == 0 && j == 1) {
+                      console.log("name:" + value1);
+                      namevalue = value1;
+                  }
+                  else if (i == 1 && j == 1) {
+                      console.log("phone:" + value1);
+                      phonevalue = value1;
+                  }
+                  else if (i == 2 && j == 1) {
+                      console.log("password:" + value1);
+                      passwordvalue = value1;
+                  }
+              });
+          });
+          data= [{customername: namevalue, phone: Number(phonevalue),password:passwordvalue ,_id: 2}];
+      }
+      else{
+          data = arry;
+          console.log("接收到的数据是一个json 格式的数组");
+      }
     const db = client.db("test");
     const pass = db.collection('register');
-    var data = [{customername: namevalue, phone: Number(phonevalue),password:passwordvalue ,_id: 2}];
-
     console.log(data);
     pass.insertMany(data, function (err, result) {
         console.log(result);
